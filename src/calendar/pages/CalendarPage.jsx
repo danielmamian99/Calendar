66353fdfd15a5513fdaf2e22711b7d6d6d464ed6
danielmamian99 <danimaster99@gmail.com>
2022-12-24 13:42:46 -0500

    delete note

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { Calendar } from "react-big-calendar";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { Navbar, CalendarEvent, CalendarModal, FabAddNew } from "../";
+import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from "../";
+
 import { localizer, getMessageES } from "../../helpers";
 import { useCalendarStore, useUiStore } from "../../hooks";
  
@@ -61,6 +62,7 @@ export const CalendarPage = () => {
       />
       <CalendarModal/>
       <FabAddNew/>
+      <FabDelete/>
     </>
   );
 };
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 
 export const useCalendarStore = () =>{
@@ -25,12 +25,19 @@ export const useCalendarStore = () =>{
         }
     }
 
+    const startDeletingEvent = async() => {
+        //TODO llegar al backend
+        await dispatch( onDeleteEvent() );
+    }
+
     return {
         //*Propiedades
         events,
         activeEvent,
+        hasEventSelected: !!activeEvent,
         //*Metodos
         setActiveEvent,
         startSavingEvent,
+        startDeletingEvent,
     }
 }
